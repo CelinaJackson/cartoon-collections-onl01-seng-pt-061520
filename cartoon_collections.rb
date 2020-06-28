65a54410d6034e96d35c34b32adc1916da0eb60b
Celina Jackson <celinanjackson@gmail.com>
2020-06-28 23:18:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)
   array = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  puts array.each  
+  index = 0 
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
 def summon_captain_planet# code an argument here
